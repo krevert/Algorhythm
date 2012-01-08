7ba7161dc7a7be144e4969fb9fac51df6980359d
Berthold Krevert <krevert@gmail.com>
2012-01-08 22:56:25 +0100

    dding support for drag&drop

--- js/loader.js
+++ js/loader.js
@@ -1,18 +1,21 @@
 /*
  * Small helper module for loading supported audio files
- * 
+ *
  * loadFile: loads first file in fileList and calls
  * callback after success and onerror after an error
  * occured
  * 
- * @author bekre
+ * requestFile: loads file given in parameter url
+ * via XMLHttp request and calls callback after success
+ * and onerror after an error occured
  * 
+ * @author bekre
  */
 
 var Loader = (function() {
-    
+
     var loader = {};
-    
+
     loader.loadFile = function(fileList, callback, onerror) {
         var reader;
         for (var i=0; i < fileList.length; i++) {
@@ -21,36 +24,38 @@ var Loader = (function() {
                 reader.onload = function(e) {
                     if (callback)
                         callback(e.target.result);
-                }   
+                }
                 reader.onerror = function() {
                     if (onerror)
                         onerror("Error while reading audio file: " + reader.error);
                 }
                 reader.readAsArrayBuffer(fileList[i]);
-                break;
+                return;
             }
         }
+        if (onerror)
+            onerror("Error: Not a valid audio file");
     }
-    
+
     loader.requestFile = function(url, callback, onerror) {
-        
+
         var request = new XMLHttpRequest();
         request.open("GET", url, true);
         request.responseType = "arraybuffer"
         request.onload = function() {
             if (callback)
                 callback(request.response);
-        }   
+        }
         request.onerror = function() {
             if (onerror)
                 onerror("Error while requesting audio file");
         }
 
         request.send();
-        
+
     }
-    
+
     return loader;
-    
+
 }());
 
