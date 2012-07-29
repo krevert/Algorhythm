8dc7996b653fb580bd6d34f964002b9857ac864c
Berthold Krevert <krevert@gmail.com>
2012-07-29 17:02:39 +0200

    Added information text and reset functionality

--- js/maggot.js
+++ js/maggot.js
@@ -1,25 +1,27 @@
 var Analyzer = (function() {
-    
+
     var DEBUG = 0;
-    
+
     var audioCtx;
     var mucke;
     var nodes;
-    
+
     var renderer;
     var scene;
     var camera;
-    
+
     var sphere;
     var vCount;
-    
+
+    var intervalId;
+
     var attributes = {
         displacement: {
             type: 'f', // a float
             value: [] // an empty array
         }
     }
-        
+
     var setSize = function(area) {
         var size = {x: window.innerWidth * 0.9 - 360, y: window.innerHeight * 0.95};
         renderer.setSize(size.x, size.y);
@@ -28,9 +30,9 @@ var Analyzer = (function() {
         area.style.width  = size.x + "px";
         area.style.height = size.y + "px";
     }
-    
+
     var init = function() {
-        
+
         audioCtx = typeof webkitAudioContext != "undefined" ? new webkitAudioContext() : null;
         mucke = null;
         nodes = new Array;
@@ -42,24 +44,28 @@ var Analyzer = (function() {
         window.addEventListener('drop', function(event) {
             event.stopPropagation();
             event.preventDefault();
+            if (!reset()) {
+                return null;
+            }
             Loader.loadFile(event.dataTransfer.files, play, printErrorMessage);
         }, false);
 
         var area = document.getElementById("renderArea");
         renderer = new THREE.WebGLRenderer();
-        renderer.setClearColorHex(0xdadce9, 1);
+        //renderer.setClearColorHex(0xdadce9, 1);
+        renderer.autoClear = false;
         area.appendChild(renderer.domElement);
-                
+
         // create a scene
         scene = new THREE.Scene();
         //var camera = new THREE.PerspectiveCamera( 45, 1.0, 1.0, 3000 );
         camera = new THREE.Camera( 45, 1.0, 1.0, 3000 );
         camera.position.z = 25;
         scene.addChild(camera);
-        
+
         setSize(area);
-        
-        // create mesh    
+
+        // create mesh
         var sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
         vCount = sphereGeometry.vertices.length;
         var myVertexShader = document.getElementById("vertexshader").innerHTML;
@@ -70,48 +76,64 @@ var Analyzer = (function() {
              fragmentShader : myFragmentShader
            }
          );
-       
+
         for (var i = 0; i < vCount; i++) {
              attributes.displacement.value[i] = Math.random();
         }
-       
+
         window.onresize = function() {
            var area = document.getElementById("renderArea");
            setSize(area);
            renderer.render(scene, camera);
         }
-       
+
         sphere = new THREE.Mesh(sphereGeometry, material);
         //sphere.scale.set(1.0, 1.0, 1.0);
         //sphere.position.set(0.0, 0.0, -25.0);
         sphere.overdraw = true;
         sphere.dynamic = true;
         scene.addChild(sphere);
-        
+
         renderer.render(scene, camera);
-        
+
     }
 
-    function processAudioData() {
+    var reset = function() {
+
+        if (nodes) {
+            for (var i=0; i < nodes.length; i++) {
+                nodes[i].disconnect();
+            }
+        }
+
+        nodes = new Array();
+        if (intervalId) {
+            window.clearInterval(intervalId);
+            intervalId = undefined;
+        }
+        return true;
+    }
+
+    var processAudioData = function() {
 
         var analyzerNode = audioCtx.createAnalyser();
         analyzerNode.fftSize = 2048;
         analyzerNode.smoothingTimeConstant = 0.75;
-        nodes.push(analyzerNode); 
+        nodes.push(analyzerNode);
 
         //bins: the sphere's vertices
         var runTo = 512;
         var binSize = Math.ceil(runTo / vCount);
-        
+
         // Cases:
         // 1) one bin for multiple coefficients
         // 2) one coefficient stretches over multiple bins (bin size = 1)
         // Displacement map can be bigger than vertices list, but we dismiss the last values
         var stretch = Math.ceil(vCount / runTo);
-        
+
         //var loop = 0;
 
-        window.setInterval(function() {
+        intervalId = window.setInterval(function() {
 
             var displacements = attributes.displacement.value;
             var data = new Uint8Array(runTo);
@@ -130,12 +152,12 @@ var Analyzer = (function() {
                 }
 
             }
-        
+
             if (DEBUG) {
                 console.log("Stretch: " + stretch + "; binSize: " + binSize);
                 console.log("attributes.displacement.value.length: " + attributes.displacement.value.length + "; sphere.vertices.length: " + vCount);
             }
-            
+
             attributes.displacement.needsUpdate = true;
             sphere.rotation.y += 0.01;
             //sphere.rotation.x += 0.01;
@@ -146,15 +168,15 @@ var Analyzer = (function() {
             //loop++;
         }, 50);
     }
-    
-    function play(rawBuffer) {
+
+    var play = function(rawBuffer) {
         var mucke = audioCtx.createBuffer(rawBuffer, false);
         var source = audioCtx.createBufferSource();
         source.buffer = mucke;
         nodes.push(source);
-        
+
         processAudioData();
-        
+
         for (var i=0; i<nodes.length; i++) {
             if (i === nodes.length - 1) {
                 nodes[i].connect(audioCtx.destination);
@@ -164,13 +186,13 @@ var Analyzer = (function() {
         }
 
         nodes[0].noteOn(0);
-        
+
     }
-    
-    function printErrorMessage(msg) {
+
+    var printErrorMessage = function(msg) {
         console.log(msg);
     }
-    
+
     return init;
-    
+
 })();
